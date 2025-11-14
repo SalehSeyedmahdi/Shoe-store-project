aab1521e0d7c884999747b50673eb634c15bfa2f
Saleh <hmohamad.sms@gmail.com>
2025-11-14 22:50:07 +0330

    feat: create first onboarding page

---
+++ src/components/onboarding/first-onboarding.js
@@ -0,0 +1,43 @@
+import { El } from "../../utils/el.js";
+
+export function FirstOnboarding() {
+	return El({
+		element: "div",
+		className:
+			"w-full h-[926px] flex flex-col items-center justify-start gap-[306px] bg-[#ffffff]",
+		children: [
+			El({
+				element: "div",
+				className: "flex justify-center items-center gap-[12px] mt-[392px]",
+				children: [
+					El({
+						element: "div",
+						className:
+							"w-[59px] h-[59px] flex justify-center items-center bg-[#000000] rounded-full",
+						children: [
+							El({
+								element: "img",
+								className: "w-[26.54px] h-[39.93px]",
+								restAttrs: {
+									src: "../../../public/images/white-logo.png",
+								},
+							}),
+						],
+					}),
+					El({
+						element: "p",
+						innerText: "Shoea",
+						className: "text-[52px] text-[#152536] font-bold",
+					}),
+				],
+			}),
+			El({
+				element: "img",
+				className: "w-[48px] h-[48px]",
+				restAttrs: {
+					src: "../../../public/images/loading.png",
+				},
+			}),
+		],
+	});
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import "./style.css";
+import { FirstOnboarding } from "../src/components/onboarding/first-onboarding.js";
+import "./style/style.css";
+
+const app = document.getElementById("app");
+app.append(FirstOnboarding());
